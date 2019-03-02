5017cd48cbd46933b0adac64a2a557b493874352
ASheehan98 <aps2@illinois.edu>
2019-03-01 23:51:35 -0600

    BasicSum done

--- Initial/src/MathPrograms/BasicSum.java
+++ Initial/src/MathPrograms/BasicSum.java
@@ -1,5 +1,22 @@
 package MathPrograms;
 
+import java.util.Scanner;
+
 public class BasicSum {
+	public static void main(String[] args) {
+		System.out.println("Enter an INTERGER");
+		Scanner first = new Scanner(System.in);
+		System.out.println("Enter a second Interger");
+		Scanner second = new Scanner(System.in);
+		System.out.println(BasicSum(first,second));
+		first.close();
+		second.close();
+		
+	}
+	public static int BasicSum(Scanner first, Scanner second) {
+		int one = first.nextInt();
+		int two = second.nextInt();
+		return(one + two);
+	}
 
 }
